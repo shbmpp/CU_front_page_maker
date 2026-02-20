1427c7d52fa2993732f03364e88bf969cb981b2d
shbmpp <shbmpp@gmail.com>
2026-02-20 22:04:02 +0530

    update

--- scripts/script.js
+++ scripts/script.js
@@ -734,7 +734,7 @@ const SUBJECTS = {
   1: makeSubjects({
     major: ["CACM", "FACM", "BRCM", "EBCM", "DTCM", "ACCM", "MSCM", "CLCM"],
     minor: ["CAGM", "FAGM", "BRGM", "EBGM", "DTGM", "ACGM", "MSGM", "CLGM",
-      "MCGM", "MPRM", "MEBS", "MMKC", "MHRM", "MMKT", "MSMT", "MPPM"
+      "MCGM", "MPRM", "MEBS", "MMKC", "MHRM", "MMKT", "MSMT", "MPPM", "ATGM"
     ]
   }, 1),
   2: makeSubjects({
@@ -1078,7 +1078,7 @@ const validatePaper = debounce(() => {
     II: { H: { "DSCC": [2], "MN": [2], "SEC": [2], "IDC": [2], "MDC": [2], "SI": [1] }, G: { /*"DSCC": [2], */ "MN": [2], "SEC": [2], "IDC": [2], "MDC": [2], "SI": [1] } },
     III: { H: { "DSCC": [3, 4], "MN": [3], "SEC": [3], "IDC": [3], "MDC": [3, 4] }, G: { /*"DSCC": [3, 4], */ "MN": [3], "SEC": [3], "IDC": [3], "MDC": [3, 4] } },
     IV: { H: { "DSCC": [5, 6, 7, 8], "MN": [4], "SI": [1] }, G: { /*"DSCC": [5, 6, 7, 8],*/ "MDC": [5, 6, 7, 8], "MN": [4], "SI": [1] } },
-    V: { H: { "DSCC": [9, 10, 11], "MN": [5, 6] }, G: { /*"DSCC": [9, 10, 11], */ "MDC": [9, 10, 11], "MN": [5, 6] } },
+    V: { H: { "DSCC": [9, 10, 11], "MN": [5, 6] }, G: { /*"DSCC": [9, 10, 11], */ "MDC": [9, 10, 11, 12], "MN": [5] } },
     VI: { H: { "DSCC": [13, 14, 15, 12], "MN": [7, 8], "SI": [1] }, G: { /*"DSCC": [13, 14, 15, 12],*/ "MDC": [13, 14, 15, 12], "MN": [7, 8], "SI": [1] } },
     VII: { H: { "DSCC": [16, 17, 18, 19, 20], "RES": [1] }, G: {} },
     VIII: { H: { "DSCC": [20, 26, 22, 23, 24, 25], "RES": [2] }, G: {} }
